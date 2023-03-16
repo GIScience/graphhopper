22a1f204381c65d8df24c26bf131a7eca1b76d46
Julian Psotta <julian.psotta@heigit.org>
2023-03-16 18:36:20 +0100

    fix(migration): Add GraphExtension

---
+++ core/src/main/java/com/graphhopper/storage/GraphExtension.java
@@ -0,0 +1,17 @@
+package com.graphhopper.storage;
+
+// TODO ORS: this is a transitional class which should be eliminated in the long run
+// ORS-GH MOD - additional class
+@SuppressWarnings("unused")
+public interface GraphExtension extends Storable<GraphExtension> {
+    GraphExtension create(long initSize);
+
+    void init(Graph graph, Directory dir);
+
+    boolean loadExisting();
+
+    void flush();
+
+    long getCapacity();
+}
+// ORS-GH MOD
